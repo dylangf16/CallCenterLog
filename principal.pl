a986d00cdfd4e6b03b20f30c1feb178c09625867
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2023-03-27 06:48:47 -0600

    ultimos arreglos

--- principal.pl
+++ principal.pl
@@ -4,11 +4,6 @@
 % inicia el proceso de ayuda al usuario, inicialmente el usuario puede
 % pedir causas, referencias, comunicar su problema o despedirse
 % inmediatamente
-hola:-inicio.
-buenos_dias:-inicio.
-buenas_tarde:-inicio.
-buenas_noches:-inicio.
-
 inicio:- write('En que puedo ayudarle? (internet, monitor, mouse, computadora, impresora, audifonos, correo, word, excel, sistema)'), nl, read(Oracion), inicio(Oracion).
 inicio(Oracion):-analyze_sentence(Oracion,_,_,Objeto,_,Causa,_,_),
     Causa\='', !, posibles_causas(Objeto), ayudar.
@@ -19,6 +14,7 @@ inicio(Oracion):-analyze_sentence(Oracion, _, Problem, Object, _, _, _, _),
     Problem\='',!, ayudar(Object).
 inicio(Oracion):-analyze_sentence(Oracion, _, _, _, _, _, _, Despedida),
     Despedida\='',!,continuar_ayuda("nada").
+inicio(Oracion) :-analyze_sentence(Oracion,Saludo,_,_,_,_,_,_),Saludo\='',!,write("Buenos días/tardes/noches!"),nl,ayudar.
 inicio(_):-write('Lo siento, no entiendo el problema, puedes decirlo de otra forma?'), read(Respuesta), inicio(Respuesta).
 
 % en caso de ocupar ayuda con un problema hace la verificacion de que
@@ -33,7 +29,7 @@ ayudar:- write("Algo mas en lo que pueda ayudarle?"), nl, read(Respuesta), conti
 continuar_ayuda(Oracion):- analyze_sentence(Oracion, _, _, _, Affirmation, _, _, _), Affirmation \= '',!, inicio.
 continuar_ayuda(Oracion):- analyze_sentence(Oracion, _, _, Problem, _, _, _, _), Problem \= '',!, inicio(Oracion).
 continuar_ayuda(Oracion):- analyze_sentence(Oracion, _, _, _, _, Causa, _, _), Causa \= '',!, inicio(Oracion).
-continuar_ayuda(Oracion):- analyze_sentence(Oracion, _, _, _, _, Referencia, _, _), Referencia \= '',!, inicio(Oracion).
+continuar_ayuda(Oracion):- analyze_sentence(Oracion, _, _, _, _, _, Referencia, _), Referencia \= '',!, inicio(Oracion).
 continuar_ayuda(_):- write("Ha sido un gusto ayudarle").
 
 
